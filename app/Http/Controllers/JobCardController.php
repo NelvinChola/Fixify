4e609232afb998305e38b57b3afa2be534186578
Derick-chola <nelvinchola.dev@outlook.com>
2025-11-11 11:26:13 +0200

    testing something

--- app/Http/Controllers/JobCardController.php
+++ app/Http/Controllers/JobCardController.php
@@ -79,14 +79,14 @@ class JobCardController extends Controller
 
 
         
-// if ($roleName === 'technician' && $request->technician_id !== $user->id) {
-//     abort(403, 'You are not authorized to view this job card.');
-// }
+if ($roleName === 'technician' && $request->technician_id !== $user->id) {
+    abort(403, 'You are not authorized to view this job card.');
+}
 
-//         // Get all technicians (you can filter by role)
-//         $technicians = User::whereHas('role', function ($q) {
-//             $q->where('name', 'technician');
-//         })->get();
+        // Get all technicians (you can filter by role)
+        $technicians = User::whereHas('role', function ($q) {
+            $q->where('name', 'technician');
+        })->get();
 
         return view('JobCard.show', compact('request', 'technicians'));
     }
